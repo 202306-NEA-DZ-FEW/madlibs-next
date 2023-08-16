dad361e694913c08df6da1381681ed49b014f272
walidbelharazem <walidbelharazem@gmail.com>
2023-08-16 14:31:49 +0200

    Done DOM

--- madlibs.js
+++ madlibs.js
@@ -57,3 +57,44 @@ return parsedStory;
 getRawStory().then(parseStory).then((processedStory) => {
   console.log(processedStory);
 });
+
+
+// DOM 
+
+function functionality(arrayOfWords) {
+  const resetBtn = document.querySelectorAll("button")[0]
+
+  //'editBox' and 'previewBox' are div elements where text is displayed.
+  const editBox = document.querySelectorAll(".editBox")[0];
+  const previewBox = document.querySelectorAll(".previewBox")[0];
+
+  //i took three inputs just to test we can add more later
+  const inputIds = ["0input-1", "1input-2", "2input-3"];
+
+  //the 'arrayOfBlanks' is set to store the user's input values for the blanks.
+  const arrayOfBlanks = []
+
+  /**
+   * 'Bella' is a young individual with no clear direction in life. However, their fervor for technology and webdev burned brightly within them. 
+   * One fateful day, an opportunity of a lifetime 'knocked' on their virtual door : an invitation to, an online 'bootcamp'.
+   */
+  const inputHolders = [
+    "Your&nbsp;name&nbsp;[n]",
+    "Put&nbsp;a&nbsp;verb&nbsp;[v]",
+    "The&nbsp;name&nbsp;of&nbsp;theworkshop&nbsp;[n]",
+  ];
+
+  let count = 0;
+
+  // Adding the story to the HTML
+   for (const obj of arrayOfWords) {
+      if (/\.\.\./.test(obj.word) !== true) {
+        editBox.innerHTML += `${obj.word} &nbsp;`;
+        previewBox.innerHTML += `${obj.word}  &nbsp;`;
+      } else {
+        editBox.innerHTML += `<input id = ${inputIds[count]} class="editable" type="text" placeholder=${inputHolders[count]} maxlength="20" autocomplete="off">`
+        previewBox.innerHTML += `<span class= ${inputIds[count]} class="readonly">[${obj.pos}] </span>`
+        count += 1
+        arrayOfBlanks.push(obj)
+      }
+    } }
